6e485264f688175f2bd698d34e9f52442f24a53d
BHAGYAshriTAMRAKAR <bt7355711982@gmail.com>
2022-05-18 00:15:45 +0530

    first commit

---
+++ Absolute_Value.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+        int absolute(int I) {
+        // Your code goes here
+        
+        if(I<0){
+            return -1*I;
+        }
+        else if(I==0) {return 0;}
+        else
+        {return I;}
+    }
+
+int main(){
+int n;
+cin>>n;
+cout<<absolute(n)<<' ';
+
+}
